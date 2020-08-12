b02067f8f8752902cd6962aa99c6cabde4c87a3b
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2020-08-12 21:10:02 +0300

    cci and tests

--- src/cci.ts
+++ src/cci.ts
@@ -1,14 +1,15 @@
 import { SMA } from './sma';
 import { MeanDeviationProvider } from './providers/mean-deviation';
 
+/**
+ * The CCI, or Commodity Channel Index, was developed by Donald Lambert,
+ * a technical analyst who originally published the indicator in Commodities magazine (now Futures)
+ * in 1980.1 Despite its name, the CCI can be used in any market and is not just for commodities
+ */
 export class CCI {
     private md: MeanDeviationProvider;
     private sma: SMA;
 
-    /**
-     * Конструктор
-     * @param period - период по умолчанию 20
-     */
     constructor(period: number = 20) {
         this.md = new MeanDeviationProvider(period);
         this.sma = new SMA(period);
@@ -18,6 +19,14 @@ export class CCI {
         const typicalPrice = (high + low + close) / 3;
         const average = this.sma.nextValue(typicalPrice);
         const meanDeviation = this.md.nextValue(typicalPrice, average);
+
+        return meanDeviation && (typicalPrice - average) / (0.015 * meanDeviation);
+    }
+
+    momentValue(high: number, low: number, close: number) {
+        const typicalPrice = (high + low + close) / 3;
+        const average = this.sma.momentValue(typicalPrice);
+        const meanDeviation = this.md.momentValue(typicalPrice, average);
 
         return meanDeviation && (typicalPrice - average) / (0.015 * meanDeviation);
     }
--- src/providers/mean-deviation.ts
+++ src/providers/mean-deviation.ts
@@ -1,17 +1,24 @@
 export class MeanDeviationProvider {
     private values: number[] = [];
-    private prevResult: number;
 
     constructor(private period: number) {}
 
     nextValue(typicalPrice: number, average?: number) {
-        if (this.values.length === this.period) {
-            this.values.shift();
+        return this.calculate(this.values, typicalPrice, average);
+    }
+
+    momentValue(typicalPrice: number, average?: number) {
+        return this.calculate(this.values.slice(0), typicalPrice, average);
+    }
+
+    calculate(values: number[], typicalPrice: number, average?: number) {
+        if (values.length === this.period) {
+            values.shift();
         }
 
-        this.values.push(typicalPrice);
-        this.prevResult = this.values.reduce((acc, value) => acc + Math.abs(average - value), 0);
+        values.push(typicalPrice);
+        const mean = values.reduce((acc, value) => acc + Math.abs(average - value), 0);
 
-        return average && this.prevResult / this.period;
+        return average && mean / this.period;
     }
 }
