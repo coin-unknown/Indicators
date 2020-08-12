bef6199179461edcef2b57349a30270eb905863a
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2020-08-12 20:34:31 +0300

    ema moment

--- src/ema.ts
+++ src/ema.ts
@@ -1,13 +1,26 @@
 import { SMA } from './sma';
 
+/**
+ * An exponential moving average (EMA) is a type of moving average (MA)
+ * that places a greater weight and significance on the most recent data points.
+ * The exponential moving average is also referred to as the exponentially weighted moving average.
+ * An exponentially weighted moving average reacts more significantly to recent price changes
+ * than a simple moving average (SMA), which applies an equal weight to all observations in the period.
+ */
 export class EMA {
     private arr: number[] = [];
     private sma: SMA;
+    private smooth: number;
 
     constructor(private period: number) {
         this.sma = new SMA(period);
+        this.smooth = 2 / (period + 1);
     }
 
+    /**
+     * Get next value for closed candle hlc
+     * affect all next calculations
+     */
     nextValue(value: number) {
         if (this.arr.length === this.period) {
             this.arr.shift();
@@ -20,16 +33,35 @@ export class EMA {
             return;
         }
 
-        const c = smooth(this.period);
-
         for (let i = this.period; i < this.arr.length; i++) {
-            ema = c * this.arr[i] + (1 - c) * ema;
+            ema = this.smooth * this.arr[i] + (1 - this.smooth) * ema;
         }
 
         return ema;
     }
-}
 
-function smooth(n: number) {
-    return 2 / (n + 1);
+     /**
+     * Get next value for non closed (tick) candle hlc
+     * does not affect any next calculations
+     */
+    momentValue(value: number) {
+        const arr = this.arr.slice(0);
+
+        if (arr.length === this.period) {
+            arr.shift();
+        }
+
+        arr.push(value);
+        let ema = this.sma.momentValue(value);
+
+        if (!ema) {
+            return;
+        }
+
+        for (let i = this.period; i < arr.length; i++) {
+            ema = this.smooth * arr[i] + (1 - this.smooth) * ema;
+        }
+
+        return ema;
+    }
 }
