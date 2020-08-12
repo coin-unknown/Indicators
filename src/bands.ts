d89d7e48d37250f96a7b97334d88e88e7361e82f
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2020-08-12 21:14:48 +0300

    bbands moment

--- src/bands.ts
+++ src/bands.ts
@@ -4,12 +4,7 @@ export class BollingerBands {
     private sd: StandardDeviationProvider;
     private sma: SMA;
 
-    /**
-     * Конструктор
-     * @param period - период по умолчанию 20
-     * @param d - множитель отклоения по умолчанию 2
-     */
-    constructor(private period: number = 20, private stdDev: number = 2) {
+    constructor(period: number = 20, private stdDev: number = 2) {
         this.sma = new SMA(period);
         this.sd = new StandardDeviationProvider(period);
     }
@@ -17,6 +12,15 @@ export class BollingerBands {
     nextValue(close: number) {
         const middle = this.sma.nextValue(close);
         const sd = this.sd.nextValue(close, middle);
+        const lower = middle - this.stdDev * sd;
+        const upper = middle + this.stdDev * sd;
+
+        return { lower, middle, upper };
+    }
+
+    momentValue(close: number) {
+        const middle = this.sma.momentValue(close);
+        const sd = this.sd.momentValue(close, middle);
         const lower = middle - this.stdDev * sd;
         const upper = middle + this.stdDev * sd;
 
--- src/providers/standard-deviation.ts
+++ src/providers/standard-deviation.ts
@@ -5,16 +5,25 @@ export class StandardDeviationProvider {
     constructor(private period: number) { }
 
     nextValue(value: number, mean?: number) {
-        if (this.values.length === this.period) {
-            this.values.shift();
+        return this.calculate(this.values, value, mean);
+    }
+
+    momentValue(value: number, mean?: number) {
+        return this.calculate(this.values.slice(0), value, mean);
+    }
+
+    calculate(values: number[], value: number, mean?: number) {
+        if (values.length === this.period) {
+            values.shift();
         }
 
-        this.values.push(value);
+        values.push(value);
 
         if (!mean) {
             return;
         }
 
-        return Math.sqrt(this.values.reduce((acc, item) => acc + (item - mean) ** 2, 0) / this.period);
+        return Math.sqrt(values.reduce((acc, item) => acc + (item - mean) ** 2, 0) / this.period);
+
     }
 }
