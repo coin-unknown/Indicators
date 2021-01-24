ef6e4ba22b430c419861b6f91a3b657d7ca23cd0
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2021-01-24 20:08:29 +0300

    dc export

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ export { RSI } from './src/rsi';
 export { CCI } from './src/cci';
 export { ATR } from './src/atr';
 export { ROC } from './src/roc';
+export { DC } from './src/dc';
 export { cRSI } from './src/crsi';
 export { BollingerBands } from './src/bands';
 export { StandardDeviation } from './src/standard-deviation';
