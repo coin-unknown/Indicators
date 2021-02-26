fbcad421917bfaa5697b29c422a73dc7f2a89048
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2021-02-26 14:18:16 +0300

    update move indicator

--- src/move.ts
+++ src/move.ts
@@ -1,64 +1,40 @@
 import { percentChange } from './utils';
 
 export class Move {
-    private prices: number[] = [];
-    private period: number;
-    private min: number;
-    private max: number;
+    private changes: number[] = [];
+    private prevPrice: number;
+    private value = 0;
 
     /**
      * Конструктор
      * @param period - целочисленное значение от 1 до  12
      * @param period - период
      */
-    constructor(period: number) {
-        this.period = period;
-    }
+    constructor(private period: number) {}
 
     nextValue(close: number) {
-        const { min, max, move } = this.calculate(close, this.prices, this.min, this.max);
 
-        this.min = min;
-        this.max = max;
+        if (this.prevPrice) {
+            const change = percentChange(close, this.prevPrice);
+            this.calculate(change);
+            this.prevPrice = close;
 
-        return move;
+            return this.value;
+        }
+
+
+        this.prevPrice = close;
     }
 
-    momentValue(close: number) {
-        const { move } = this.calculate(close, this.prices.slice(0), this.min, this.max);
-
-        return move;
-    }
-
-    calculate(close: number, prices: number[], min: number, max: number) {
-        let removed: number;
-
-        if (prices.length === this.period) {
-            removed = prices.shift();
+    calculate(change: number) {
+        if (this.changes.length >= this.period) {
+            const rm = this.changes.shift();
+            this.value -= rm;
         }
 
-        prices.push(close);
+        this.value += change;
+        this.changes.push(change);
 
-        if (prices.length < this.period) {
-            return {};
-        }
-
-        if (close > max || !max) {
-            max = close;
-        }
-
-        if (min > close || !min) {
-            min = close;
-        }
-
-        if (removed === max) {
-            max = Math.max(...prices);
-        } else if (removed == min) {
-            min = Math.min(...prices);
-        }
-
-        const p = close > min ? percentChange(close, min) : percentChange(close, max);
-
-        return { min, max, move: p };
+        return this.value;
     }
 }
