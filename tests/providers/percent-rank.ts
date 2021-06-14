d3b00c574241f4e7046a8ce93b463b544a2d7672
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2021-06-14 15:20:42 +0300

    crsi annotation

--- tests/providers/percent-rank.ts
+++ tests/providers/percent-rank.ts
@@ -2,7 +2,7 @@ import { PercentRank } from '../../src/providers/percent-rank';
 
 const percentRank = new PercentRank(10);
 
-[100, 5, 20, 50, 55, 60, 70, 80, 90, 1].forEach(v => {
+[100, 5, 20, 50, 55, 60, 70, 80, 90, 1].forEach((v) => {
     percentRank.nextValue(v);
 });
 
