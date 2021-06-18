758b2804d92a12303bff31d4ea2e9d01d74bf1c8
voqse <git@voqse.com>
2021-06-18 22:31:06 +0700

    Add MACD signal line

--- src/macd.ts
+++ src/macd.ts
@@ -8,23 +8,28 @@ https://www.investopedia.com/terms/m/macd.asp#:~:text=The%20MACD%20is%20calculat
 export class MACD {
     private emaFastIndicator: EMA;
     private emaSlowIndicator: EMA;
+    private emaSignalIndicator: EMA;
     private emaFast: number;
     private emaSlow: number;
+    private signal: number;
     private macd: number;
 
-    constructor(private periodEmaFast: number, private periodEmaSlow: number) {
+    constructor(private periodEmaFast = 12, private periodEmaSlow = 26, private periodSignal = 9) {
         this.emaFastIndicator = new EMA(periodEmaFast);
         this.emaSlowIndicator = new EMA(periodEmaSlow);
+        this.emaSignalIndicator = new EMA(periodSignal);
     }
 
     nextValue(value: number) {
         this.emaFast = this.emaFastIndicator.nextValue(value);
         this.emaSlow = this.emaSlowIndicator.nextValue(value);
         this.macd = this.emaFast - this.emaSlow;
+        this.signal = this.emaSignalIndicator.nextValue(this.macd);
         return {
             macd: this.macd,
-            emaFast : this.emaFast,
-            emaSlow: this.emaSlow
-        }
+            emaFast: this.emaFast,
+            emaSlow: this.emaSlow,
+            signal: this.signal,
+        };
     }
 }
