dd0a051f80276a5925e15f8bc5edd58668518a95
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2021-06-26 16:33:10 +0300

    heiken ashi indicator

---
+++ src/heiken-ashi.ts
@@ -0,0 +1,53 @@
+/**
+ * Heikin-Ashi Candlesticks are an offshoot from Japanese candlesticks.
+ * Heikin-Ashi Candlesticks use the open-close data from the prior period
+ * and the open-high-low-close data from the current period to create a combo candlestick.
+ * The resulting candlestick filters out some noise in an effort to better capture the trend.
+ * In Japanese, Heikin means “average” and Ashi means “pace” (EUDict.com).
+ * Taken together, Heikin-Ashi represents the average pace of prices.
+ * Heikin-Ashi Candlesticks are not used like normal candlesticks.
+ * Dozens of bullish or bearish reversal patterns consisting of 1-3 candlesticks are not to be found.
+ * Instead, these candlesticks can be used to identify trending periods,
+ * potential reversal points and classic technical analysis patterns.
+ */
+export class HeikenAshi {
+    private prevOpen = 0;
+    private prevClose = 0;
+
+    /**
+     * Get next value for closed candle hlc
+     * affect all next calculations
+     */
+    nextValue(o: number, h: number, l: number, c: number) {
+        const data = this.calculate(o, h, l, c);
+
+        this.prevClose = data.c;
+        this.prevOpen = data.o;
+
+        return data;
+    }
+
+    /**
+     * Get next value for non closed (tick) candle hlc
+     * does not affect any next calculations
+     */
+    momentValue(o: number, h: number, l: number, c: number) {
+        return this.calculate(o, h, l, c);
+    }
+
+    /**
+     * Heiken ashi formula
+     */
+    calculate(o: number, h: number, l: number, c: number) {
+        if (!this.prevOpen) {
+            return { o, h, l, c };
+        }
+
+        const close = (o + h + l + c) / 4;
+        const open = (this.prevOpen + this.prevClose) / 2;
+        const high = Math.max(h, open, close);
+        const low = Math.min(l, open, close);
+
+        return { o: open, h: high, l: low, c: close };
+    }
+}
