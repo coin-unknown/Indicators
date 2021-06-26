f976a3d8a4d8638c0e13bdfe132c14ca2362a819
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2021-06-26 16:34:01 +0300

    add ha to export

--- index.ts
+++ index.ts
@@ -15,3 +15,4 @@ export { cRSI } from './src/crsi';
 export { BollingerBands } from './src/bands';
 export { StandardDeviation } from './src/standard-deviation';
 export { MACD } from './src/macd';
+export { HeikenAshi } from './src/heiken-ashi';
