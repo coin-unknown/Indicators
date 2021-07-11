9c758cc6063c8939af7eb699f9eb5fea818d1924
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2021-07-11 19:43:17 +0300

    ha cros validate tets with issues

--- tests/heikenashi/ha.spec.ts
+++ tests/heikenashi/ha.spec.ts
@@ -1,5 +1,6 @@
 import { haValues, ohlc } from './excel-data';
 import { HeikenAshi } from '../../src/heiken-ashi';
+import { HeikinAshi as HeikenAshi2 } from 'technicalindicators';
 
 describe('Heiken Ashi', () => {
     it('Excel Validate', () => {
@@ -14,6 +15,21 @@ describe('Heiken Ashi', () => {
             expect(Math.abs(calculated.h - excel.h)).toBeLessThan(EPSILON);
             expect(Math.abs(calculated.l - excel.l)).toBeLessThan(EPSILON);
             expect(Math.abs(calculated.c - excel.c)).toBeLessThan(EPSILON);
+        });
+    });
+
+    it.skip('Cross SDK Validate', () => {
+        const ha = new HeikenAshi();
+        const ha2 = new HeikenAshi2({ open: [], high: [], low: [], close: [] });
+
+        ohlc.forEach((tick) => {
+            const calculated = ha.nextValue(tick.o, tick.h, tick.l, tick.c);
+            const cross = ha2.nextValue({ open: tick.o, high: tick.h, low: tick.l, close: tick.c });
+
+            expect(calculated.o).toEqual(cross.open);
+            expect(calculated.h).toEqual(cross.high);
+            expect(calculated.l).toEqual(cross.low);
+            expect(calculated.c).toEqual(cross.close);
         });
     });
 });
