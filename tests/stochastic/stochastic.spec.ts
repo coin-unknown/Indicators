eedcd25cc201029266f31d75122eb27c9122733d
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2021-07-12 11:34:07 +0300

    stochastic unit test

---
+++ tests/stochastic/stochastic.spec.ts
@@ -0,0 +1,52 @@
+import { Stochastic } from '../../src/stochastic';
+import { Stochastic as Stochastic2 } from 'technicalindicators';
+import { ohlc, stochValues } from './excel-data';
+
+describe('Stochastic Oscillator', () => {
+    it('Excel Validate', () => {
+        const st = new Stochastic();
+        const EPSILON = 0.0001;
+
+        ohlc.forEach((tick, idx) => {
+            const calculated = st.nextValue(tick.h, tick.l, tick.c);
+            const excel = stochValues[idx];
+
+            if (calculated?.k && excel?.k) {
+                expect(Math.abs(calculated?.k - excel?.k)).toBeLessThan(EPSILON);
+            }
+
+            if (calculated?.d && excel?.d) {
+                expect(Math.abs(calculated?.d - excel?.d)).toBeLessThan(EPSILON);
+            }
+        });
+    });
+
+    it('Cross Validate', () => {
+        ohlc.forEach((tick) => {
+            const st = new Stochastic();
+            const st2 = new Stochastic2({
+                period: 14,
+                low: [],
+                high: [],
+                close: [],
+                signalPeriod: 3,
+            });
+            const calculated = st.nextValue(tick.h, tick.l, tick.c);
+            const cross = st2.nextValue({
+                high: [tick.h],
+                low: [tick.l],
+                close: [tick.c],
+                period: 14,
+                signalPeriod: 3,
+            });
+
+            if (calculated?.k && cross?.k) {
+                expect(calculated?.k).toEqual(cross?.k);
+            }
+
+            if (calculated?.d && cross?.d) {
+                expect(calculated?.d).toEqual(cross?.d);
+            }
+        });
+    });
+});
