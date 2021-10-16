0d99634082c052fa5aa6657675320fc56f382f05
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2021-10-16 21:09:29 +0300

    stable result with new logic without extremums passing

--- src/trendlines/index.ts
+++ src/trendlines/index.ts
@@ -1,61 +1,79 @@
-import { Extremums } from '../providers/extremum';
+import { Extremums } from './extremum';
 import { HighLine, LowLine } from './line';
 import { ExtremumsItem, LineId, LineEvent } from './types';
 
 export class TrendLines {
-    private turningPoints = [];
     private lines: Array<HighLine | LowLine> = [];
     private hLine: HighLine;
     private lLine: LowLine;
-    private highExtremumGetter: Extremums;
-    private lowExtremumGetter: Extremums;
-    private highExtremums: ExtremumsItem[] = [];
-    private lowExtremums: ExtremumsItem[] = [];
+    private extremumGetter: Extremums;
     private highExtremum: ExtremumsItem | null = null;
     private lowExtremum: ExtremumsItem | null = null;
     private i = 0;
-    private breakCounter = 0;
+    private waitNext: 'hLine' | 'lLine' = 'hLine';
     constructor() {
-        this.highExtremumGetter = new Extremums('max');
-        this.lowExtremumGetter = new Extremums('min');
+        this.extremumGetter = new Extremums();
     }
 
     nextValue(o: number, c: number) {
         const max = o >= c ? o : c;
         const min = o >= c ? c : o;
-        const hExtremum = this.highExtremumGetter.nextValue(max);
-        const lExtremum = this.lowExtremumGetter.nextValue(max);
 
-        if (hExtremum) {
-            const extremumPoint = { idx: this.i - 1, value: hExtremum };
+        this.extremumGetter.updateMax(max, this.i);
+        this.extremumGetter.updateMin(min, this.i);
 
-            this.highExtremums.push(extremumPoint);
-
-            // Условия переопределения экстремума
-            if (!this.highExtremum) {
-                this.highExtremum = extremumPoint;
-            }
+        // Генератор линий поддержки
+        if (this.waitNext === 'hLine') {
+            this.createHighLine(max);
         }
 
-        if (lExtremum) {
-            const extremumPoint = { idx: this.i - 1, value: lExtremum };
-
-            this.lowExtremums.push(extremumPoint);
+        // Генератор линий сопротивления
+        if (this.waitNext === 'lLine') {
+            this.createLowLine(min);
         }
 
         let result: number[];
 
-        if (this.highExtremum) {
-            result = this.produceHigh(max);
+        if (this.hLine) {
+            const event = this.hLine.update(max, this.i);
+
+            if (event === LineEvent.BREAKDOWN) {
+                // Пробой, удаляем активную линию сопротивления
+                this.highExtremum = null;
+                this.hLine = null;
+                this.waitNext = 'lLine';
+                this.createLowLine(min);
+            } else {
+                result = [
+                    this.hLine.valueAtPoint(this.i),
+                    HighLine.minK * this.i +
+                        (this.highExtremum.value - HighLine.minK - HighLine.minK * this.highExtremum.idx),
+                    undefined,
+                    undefined,
+                    this.hLine.getSubtrendValue(this.i),
+                ];
+            }
         }
 
-        // Условия переопределения экстремума
-        if (this.breakCounter >= 6 && !this.lowExtremum) {
-            this.lowExtremum = this.lowExtremums.slice(-1)[0];
-        }
+        if (this.lLine) {
+            const event = this.lLine.update(min, this.i);
 
-        if (this.lowExtremum) {
-            result = (result || []).concat(this.produceLow(min));
+            if (event === LineEvent.BREAKDOWN) {
+                this.lLine = null;
+                this.lowExtremum = null;
+                this.waitNext = 'hLine';
+                this.createHighLine(max);
+            } else {
+                result = [
+                    undefined,
+                    undefined,
+                    this.lLine.valueAtPoint(this.i),
+                    LowLine.minK * this.i +
+                        (this.lowExtremum.value - LowLine.minK - LowLine.minK * this.lowExtremum.idx),
+                    undefined,
+                    this.lLine.getSubtrendValue(this.i),
+                ];
+            }
         }
 
         this.i++;
@@ -63,65 +81,50 @@ export class TrendLines {
         return result;
     }
 
-    private produceHigh(max: number) {
-        // Строим первую линию
-        // Высчитываем прямую k, b
-        // const prevPoint = array[i - 1];
-        let breakdown = 0;
+    private createHighLine(max: number) {
+        const extremum = this.extremumGetter.getMax();
+
+        if (!extremum) {
+            return false;
+        }
+
+        // Условия переопределения экстремума
         //  1ая итерация, k,b - неизвестны
-        if (!this.hLine) {
-            const k = (max - this.highExtremum.value) / (this.i - this.highExtremum.idx);
-            const b = max - k * this.i;
+        const k = (max - extremum.value) / (this.i - extremum.idx);
+        const b = max - k * this.i;
 
-            // Нормальные условия линии
-            if (k < HighLine.minK) {
-                const line = new HighLine(k, b, this.i);
-                this.hLine = line;
-                this.lines.push(line);
-            } else {
-                return;
-            }
+        // Нормальные условия линии
+        if (k < HighLine.minK) {
+            const line = new HighLine(k, b, extremum.idx);
+            this.hLine = line;
+            this.lines.push(line);
+            this.highExtremum = extremum;
+            this.waitNext = null;
+            this.extremumGetter.resetMin();
+            return true;
         }
-
-        const hEvent = this.hLine.update(max, this.i, this.highExtremum);
-
-        if (hEvent === LineEvent.BREAKDOWN) {
-            breakdown = 1;
-            this.breakCounter++;
-        }
-
-        return [
-            this.hLine.valueAtPoint(this.i),
-            HighLine.minK * this.i + (this.highExtremum.value - HighLine.minK - HighLine.minK * this.highExtremum.idx),
-            breakdown,
-        ];
     }
 
-    produceLow(min: number) {
-        if (!this.lLine) {
-            const k = (min - this.lowExtremum.value) / (this.i - this.lowExtremum.idx);
-            const b = min - k * this.i;
+    private createLowLine(min: number) {
+        const extremum = this.extremumGetter.getMin();
 
-            // Нормальные условия линии
-            if (k > LowLine.minK) {
-                const line = new LowLine(k, b, this.i);
-                this.lLine = line;
-                this.lines.push(line);
-            } else {
-                return;
-            }
+        if (!extremum) {
+            return false;
         }
 
-        const event = this.lLine.update(min, this.i, this.lowExtremum);
+        //  1ая итерация, k,b - неизвестны
+        const k = (min - extremum.value) / (this.i - extremum.idx);
+        const b = min - k * this.i;
 
-        if (event === LineEvent.BREAKDOWN) {
-            // breakdown = 1;
-            // this.breakCounter++;
+        // Нормальные условия линии
+        if (k > LowLine.minK) {
+            const line = new LowLine(k, b, extremum.idx);
+            this.lLine = line;
+            this.lines.push(line);
+            this.lowExtremum = extremum;
+            this.waitNext = null;
+            this.extremumGetter.resetMax();
+            return true;
         }
-
-        return [
-            this.lLine.valueAtPoint(this.i),
-            LowLine.minK * this.i + (this.lowExtremum.value - LowLine.minK - LowLine.minK * this.lowExtremum.idx),
-        ];
     }
 }
--- src/trendlines/types.ts
+++ src/trendlines/types.ts
@@ -2,6 +2,7 @@ export type LineId = string;
 export type ExtremumsItem = { idx: number; value: number };
 export enum LineEvent {
     'BREAKDOWN',
+    'WAIT_SMOOTH',
     'SMOOTH',
     'BOUNCE',
 }
