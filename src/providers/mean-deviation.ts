22b28c9baebc60fc94adaba170261ce85e00c66d
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2021-11-03 12:50:29 +0300

    purify cci

--- src/providers/mean-deviation.ts
+++ src/providers/mean-deviation.ts
@@ -7,19 +7,37 @@ export class MeanDeviationProvider {
     }
 
     nextValue(typicalPrice: number, average?: number) {
-        this.values.push(typicalPrice);
+        if (!average) {
+            this.values.push(typicalPrice);
+            return void 0;
+        }
 
-        const mean = this.values.toArray().reduce((acc, value) => acc + Math.abs(average - value), 0);
+        this.nextValue = this.pureNextValue;
+        this.momentValue = this.pureMomentValue;
 
-        return average && mean / this.period;
+        return this.pureNextValue(typicalPrice, average);
     }
 
     momentValue(typicalPrice: number, average?: number) {
+        return void 0;
+    }
+
+    private pureNextValue(typicalPrice: number, average: number) {
+        this.values.push(typicalPrice);
+
+        return this.values.toArray().reduce((acc, value) => acc + this.positiveDelta(average, value), 0) / this.period;
+    }
+
+    private pureMomentValue(typicalPrice: number, average: number) {
         const rm = this.values.push(typicalPrice);
-        const mean = this.values.toArray().reduce((acc, value) => acc + Math.abs(average - value), 0);
+        const mean = this.values.toArray().reduce((acc, value) => acc + this.positiveDelta(average, value), 0);
 
         this.values.pushback(rm);
 
-        return average && mean / this.period;
+        return mean / this.period;
+    }
+
+    private positiveDelta(a: number, b: number) {
+        return a > b ? a - b : b - a;
     }
 }
