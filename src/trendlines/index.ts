3750d21cd5b37f4303f48b646f85f9637ed83d96
stipot <inimatic@users.noreply.github.com>
2021-12-20 03:28:21 +0300

    feat: initial step to new tranding model

--- src/trendlines/index.ts
+++ src/trendlines/index.ts
@@ -11,13 +11,30 @@ export class TrendLines {
     private lowExtremum: ExtremumsItem | null = null;
     private i = 0;
     private waitNext: 'hLine' | 'lLine' = 'hLine';
+    private tLines: LineModel[]
+    private step: number = 1
     constructor() {
         this.extremumGetter = new Extremums();
     }
     //Next value
-    nextValue(o: number, c: number) {
-        const max = o >= c ? o : c;
-        const min = o >= c ? c : o;
+    nextValue(o: number, c: number, h: number, l: number) {
+        //New model. Отличия:
+        //Анализ по h/l, а не по max/min
+        //Поиск ускорений после потенциального отскока
+        //Одновременный поиск трендов сверху и снизу
+        const max = h; //o >= c ? o : c;
+        const min = l; //o >= c ? c : o;
+        //init tLines
+        if (!this.tLines) {
+            //TODO Load i from candels data
+            let tLine = new LineModel(h, null, this.i, this.step)
+            this.tLines.push(tLine)
+            tLine = new LineModel(null, l, this.i, this.step)
+            this.tLines.push(tLine)
+        }
+        this.tLines.forEach(tline => tline.update(h, l, this.i))
+
+        // Old model
 
         // TODO: Исключить. Перенести в прямую
         this.extremumGetter.updateMax(max, this.i);
@@ -133,4 +150,76 @@ export class TrendLines {
             this.extremumGetter.resetMax();
         }
     }
+}
+
+export class Point {
+    y: number
+    x: number
+}
+export interface LineDirective {
+    condition: 'lt' | 'gt' | 'lgt'
+    value: number
+    action: string
+}
+export class LineModel {
+    private type = 'h' || 'l'
+    private startPoint: Point
+    private prevPoint: Point
+    private nextPoint: Point
+    private curPoint: Point
+    private k: number
+    private b: number
+    private step: number //Шаг времени в минутах
+    private error: number
+    constructor(h, l, i, step) {
+        this.step = step
+        this.init(h, l, i)
+        this.startPoint = this.curPoint
+    }
+    init(h, l, i) {
+        this.type = h ? 'h' : 'l'
+        this.curPoint = {
+            y: h || l,
+            x: i
+        }
+        this.prevPoint = this.curPoint
+    }
+
+    /**
+     * Пусть минимальный временной шаг в минутах. Но данные могу поступать с разнуми интервалами. Поэтому t имеет значение.
+     * На второй точке получаем К
+     * Если точка близка к линии - выделяем точку ка индикатор
+     * Если текущая точка пробивает линию перестраиваемся
+     * Если перестройка ведкт к К меньше К предыдущей линии - сигнал
+     * Если перестройка ведет к смене знака К - сигнал (перелом тренда). Может возникнуть только на fork
+     * @param h
+     * @param l
+     * @param i
+     */
+    update(h, l, i): LineDirective {
+        this.init(h, l, i)
+        // инициализация К b
+        this.k ??= (this.curPoint.y - this.startPoint.y) / (this.curPoint.x - this.startPoint.x)
+        this.b ??= this.curPoint.y - this.k * this.curPoint.x
+        //Update incline
+        if ((this.type == 'h' && this.nextPoint.y > this.curPoint.y) || (this.type == 'l' && this.nextPoint.y < this.curPoint.y)) {
+            this.k ??= (this.curPoint.y - this.startPoint.y) / (this.curPoint.x - this.startPoint.x)
+            this.b ??= this.curPoint.y - this.k * this.curPoint.x
+        }
+        //Update next point
+        this.nextPoint = {
+            y: this.k * (this.curPoint.x + this.step) + this.b,
+            x: i + this.step
+        }
+        return {
+            condition: this.type == 'h' ? 'lt' : 'gt',
+            value: this.nextPoint.y,
+            action: 'fork'
+        }
+    }
+
+    updateStep(step) {
+        this.step = step
+    }
+
 }
