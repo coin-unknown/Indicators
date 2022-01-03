3c421be62d40b4311687796b2a4e57a3ee402dc2
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2022-01-03 23:40:20 +0300

    super trend fixes

--- src/supertrend.ts
+++ src/supertrend.ts
@@ -1,48 +1,74 @@
 import { ATR } from './atr';
-import { CCI } from './cci';
 /**
- * The MTF SuperTrend indicator is one of the hybrid custom tools that show the current trend in the market.
+ * SuperTrend indicator is one of the hybrid custom tools that show the current trend in the market.
  * The indicator name stands for Multi Time Frame SuperTrend.
  * The tool can show the direction of the trend on several timeframes at once.
  */
 export class SuperTrend {
-    private cci: CCI;
     private atr: ATR;
+    private prevSuper: number;
+    private prevUpper: number;
+    private prevLower: number;
+    private prevClose: number;
 
-    /**
-     * @param cciPeriod recomended 50
-     * @param atrPeriod recommended 5
-     */
-    constructor(cciPeriod = 50, atrPeriod = 5) {
-        this.atr = new ATR(atrPeriod);
-        this.cci = new CCI(cciPeriod);
+    constructor(period = 10, private multiplier = 3) {
+        this.atr = new ATR(period);
     }
 
     nextValue(h: number, l: number, c: number) {
-        const cci = this.cci.nextValue(h, l, c);
         const atr = this.atr.nextValue(h, l, c);
 
-        if (cci !== undefined) {
-            this.nextValue = (h: number, l: number, c: number) => {
-                const cci = this.cci.nextValue(h, l, c);
-                const atr = this.atr.nextValue(h, l, c);
-                const sh = h + atr;
-                const sl = l - atr;
+        if (atr) {
+            const src = (h + l) / 2;
+            let upper = src + this.multiplier * atr;
+            let lower = src - this.multiplier * atr;
 
-                return { sh, sl, st: cci > 0 ? sh : sl, cci };
-            };
+            if (this.prevLower) {
+                lower = lower > this.prevLower || this.prevClose < this.prevLower ? lower : this.prevLower;
+                upper = upper < this.prevSuper || this.prevClose > this.prevUpper ? upper : this.prevUpper;
+            }
 
-            const sh = h + atr;
-            const sl = l - atr;
+            let superTrend = upper;
 
-            return { sh, sl, st: cci > 0 ? sh : sl, cci };
+            if (this.prevSuper === this.prevUpper) {
+                superTrend = c > upper ? lower : upper;
+            } else {
+                superTrend = c < lower ? upper : lower;
+            }
+
+            const direction = superTrend === upper ? 1 : -1;
+
+            this.prevUpper = upper;
+            this.prevLower = lower;
+            this.prevSuper = superTrend;
+            this.prevClose = c;
+
+            return { upper, lower, superTrend, direction };
         }
     }
 
     momentValue(h: number, l: number, c: number) {
-        const cci = this.cci.momentValue(h, l, c);
         const atr = this.atr.momentValue(h, l);
+        const src = (h + l) / 2;
 
-        return cci > 0 ? h + atr : l - atr;
+        let upper = src + this.multiplier * atr;
+        let lower = src - this.multiplier * atr;
+
+        if (this.prevLower) {
+            lower = lower > this.prevLower || this.prevClose < this.prevLower ? lower : this.prevLower;
+            upper = upper < this.prevSuper || this.prevClose > this.prevUpper ? upper : this.prevUpper;
+        }
+
+        let superTrend = upper;
+
+        if (this.prevSuper === this.prevUpper) {
+            superTrend = c > upper ? lower : upper;
+        } else {
+            superTrend = c < lower ? upper : lower;
+        }
+
+        const direction = superTrend === upper ? 1 : -1;
+
+        return { upper, lower, superTrend, direction };
     }
 }
