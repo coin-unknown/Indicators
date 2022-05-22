3d67f3751288eeb0f182339b9bb2fdf5ea85bc9c
Dmitriy Yurov <narkozzforb@gmail.com>
2022-05-22 19:49:18 +0300

    fixes zero values with equals to undefined (#26)
    Co-authored-by: Dmitriy Yurov <dyurov@tradingview.com>

--- src/adx.ts
+++ src/adx.ts
@@ -64,7 +64,7 @@ export class ADX {
         this.prevLow = l;
         this.prevClose = c;
 
-        if (!avgPDI || !avgNDI) {
+        if (avgPDI === undefined || avgNDI === undefined) {
             return;
         }
 
