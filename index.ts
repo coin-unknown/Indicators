89fd5d5105f3c670b32751a650e6a830ef098cc1
StaRenn <starendev@gmail.com>
2022-07-10 20:07:43 +0300

    prettier

--- index.ts
+++ index.ts
@@ -33,4 +33,4 @@ export { WWS } from './src/wws';
 export { SuperTrend } from './src/supertrend';
 export { CircularBuffer } from './src/providers/circular-buffer';
 export { Sampler } from './src/providers/sampler';
-export {VolumeProfile, VolumeTypes, PriceSource} from './src/volume-profile'
+export { VolumeProfile, VolumeTypes, PriceSource } from './src/volume-profile';
