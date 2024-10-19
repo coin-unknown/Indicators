d047b2a9ef33ca914fb1fe4a3c138e1f6cdaa4d8
Dmitriy Yurov <narkozzforb@gmail.com>
2024-10-19 13:01:22 +0200

    fix test

--- tests/ama/ama.spec.ts
+++ tests/ama/ama.spec.ts
@@ -3,14 +3,14 @@ import { data } from './data';
 
 describe('Adaptive Moving Average', () => {
     const ama = new AMA(15, 2, 30);
-    const EPSILON = 0.01;
+    const EPSILON = 0.001;
 
     it('Excel validate', () => {
         data.forEach((tick, idx) => {
             const amaValue = ama.nextValue(tick.c);
             const expected = Number(tick.ama);
 
-            if (amaValue) {
+            if (amaValue && expected) {
                 expect(Math.abs(amaValue - expected)).toBeLessThan(EPSILON);
             }
 
